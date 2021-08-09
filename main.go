44e481da09f535d752caa2cecc6795c986364fb9
enylvia <adityaperm06@gmail.com>
2021-08-09 17:47:14 +0700

    Input from Service

--- main.go
+++ main.go
@@ -3,45 +3,29 @@ package main
 import (
 	"crowdfund-go/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 
-	// dsn := "root:@tcp(127.0.0.1:3306)/crowdfund?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database execute")
-
-	// var users []user.User
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// }
-	router := gin.Default()
-	router.GET("/", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfund?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
-	}
-	var users []user.User
-	db.Find(&users)
 
-	c.JSON(http.StatusOK, users)
+	}
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userInput := user.RegisterUserInput{}
+
+	userInput.Name = "Test Simpan dari service"
+	userInput.Email = "contoh@mail.com"
+	userInput.Occupation = "pelajar"
+	userInput.Password = "12345678"
+
+	userService.RegisterUser(userInput)
 }
